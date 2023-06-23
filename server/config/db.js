5d2e305e3983d31afb314e2aaa118dbbb199b8f2
Bhamid4 <113592135+Bhamid4@users.noreply.github.com>
2023-06-23 17:51:06 -0400

    final commit

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+
+module.exports = 
+{
+    
+    //"URI": "mongodb://localhost/my_express_site"
+  "URI": "mongodb+srv://BilalHamid:B123456789l@mongodbserver.opav6jv.mongodb.net/my_business_contects?retryWrites=true&w=majority"
+}
