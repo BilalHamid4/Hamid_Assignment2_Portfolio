f67f3735860f079f96ba88098c5e24e1743b43af
Bhamid4 <113592135+Bhamid4@users.noreply.github.com>
2023-06-24 15:30:25 -0400

    server changed to 3001

--- server/config/db.js
+++ server/config/db.js
@@ -3,5 +3,5 @@ module.exports =
 {
     
     //"URI": "mongodb://localhost/my_express_site"
-  "URI": "mongodb+srv://BilalHamid:B123456789l@mongodbserver.opav6jv.mongodb.net/my_business_contects?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://BilalHamid:B1234567890l@mongodbserver.opav6jv.mongodb.net/my_business_contects?retryWrites=true&w=majority"
 }
